c4c2a60d91378b2509213ac45dfdea5e5a9652f6
shinuamai <laurad00sandovalg@gmail.com>
2023-12-11 13:20:06 -0500

    first commit

--- applications/EscolarSystem/languages/es.py
+++ applications/EscolarSystem/languages/es.py
@@ -55,7 +55,6 @@
 'Community': 'Comunidad',
 'Components and Plugins': 'Componentes y Plugins',
 'Config.ini': 'Config.ini',
-'Confirm Password': 'Confirm Password',
 'Controller': 'Controlador',
 'Copyright': 'Copyright',
 'Current request': 'Solicitud en curso',
